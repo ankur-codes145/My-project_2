06d221ac2fac7b4305f0ca7cc6e5889a5cbc5081
Ankur Gangwar <ankurgangwar645@gmail.com>
2026-02-05 21:21:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
@@ -8,95 +8,101 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError");
+const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require('connect-mongo'); 
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user");
+const User = require("./models/user.js");
 
-const listingRouter = require("./routes/listing");
-const reviewRouter = require("./routes/review");
-const userRouter = require("./routes/user");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
 
-// ===== MongoDB Connection =====
-mongoose
-  .connect(dbUrl)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("Mongo error:", err));
+const  dbUrl = process.env.ATLASDB_URL;
 
-// ===== View Engine =====
-app.engine("ejs", ejsMate);
+main()
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-// ===== Middleware =====
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-// ===== Session Store =====
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: { secret: process.env.SECRET },
-  touchAfter: 24 * 3600,
-});
+ mongoUrl: dbUrl,
+ crypto: {
+  secret: process.env.SECRET,
+ },
+ touchAfter: 24 * 3600,
+}) ;
 
 store.on("error", (err) => {
-  console.log("SESSION STORE ERROR", err);
+  console.log("error in MONGO SESSION STORE", err);
 });
 
-const sessionConfig = {
+const sessionOptions = {
   store,
-  name: "session",
   secret: process.env.SECRET,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: true, 
   cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+ secure: process.env.NODE_ENV === "production",
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
   },
 };
 
-app.use(session(sessionConfig));
+
+
+
+
+app.use(session(sessionOptions));
 app.use(flash());
 
-// ===== Passport =====
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// ===== Global Variables =====
 app.use((req, res, next) => {
   res.locals.currUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  
   next();
 });
 
-// ===== Routes =====
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// ===== 404 =====
 app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"));
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
-// ===== Error Handler =====
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Something went wrong" } = err;
+  let { statusCode = 500, message = "something went wrong!" } = err;
   res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
-// ❌ NO app.listen() FOR VERCEL
-
-module.exports = app;
+app.listen(8080, () => {
+  console.log("server is listening on 8080");
+});
